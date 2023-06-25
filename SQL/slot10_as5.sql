f3178e9e91430f8ca214d54f1fa1c1621e04e5ac
MinhMVT <129242261+MinhMVT@users.noreply.github.com>
2023-06-25 21:02:42 +0700

    s

--- SQL/slot10_as5.sql
+++ SQL/slot10_as5.sql
@@ -13,4 +13,64 @@ create table DienThoai (
 	foreign key (ma_nguoi) references DanhBa(ma_nguoi)
 );
 --3: Thêm dữ liệu vào bảng
-insert into DanhBa values
+insert into DanhBa (ma_nguoi, ho_va_ten, dia_chi, ngay_sinh) values (1, N'Nguyễn Văn An', N'111 Nguyễn Trãi, Thanh Xuân, Hà Nội', '11/18/87'),
+																	(2, N'Nguyễn Văn Tùng', N'Xóm 2, Đông La, Hoài Đức, Hà Nội', '09/29/95'),
+																	(3, N'Đinh Tiến Dũng', N'Đâu đó trên trái đất', '02/03/80');
+insert into DienThoai (ma_nguoi, so_dien_thoai) values (1, '987654321'),
+													   (1, '09873452'),
+													   (1, '09832323'),
+													   (1, '09434343'),
+													   (2, '0379113133'),
+													   (3, '0124151513'),
+													   (3, '0365131616'),
+													   (3, '0125346215');
+--4a: Liệt kê những người có trong danh bạ
+select ho_va_ten, dia_chi, ngay_sinh from DanhBa;
+--4b: Danh sách số điện thoại có trong danh bạ
+select ho_va_ten ,so_dien_thoai from DienThoai dt join DanhBa db on dt.ma_nguoi = db.ma_nguoi;
+--5a: Liệt kê danh sách người trong danh bạ theo thứ tự alphabet
+select ho_va_ten, dia_chi, ngay_sinh from DanhBa order by ho_va_ten asc;
+--5b: Liệt kê số điện thoại của người có tên Nguyễn Văn An
+select ho_va_ten, so_dien_thoai from DienThoai dt join DanhBa db on dt.ma_nguoi = db.ma_nguoi where ho_va_ten like N'Nguyễn Văn An';
+--5c: Liệt kê những người có ngày sinh là 12/12/09
+select * from DanhBa where ngay_sinh = '12/12/09';
+--6a: Tìm số lượng số điện thoại của mỗi người trong danh bạ
+select ho_va_ten, count(so_dien_thoai) as so_luong from DanhBa db join DienThoai dt on dt.ma_nguoi = db.ma_nguoi group by ho_va_ten;
+--6b: Tìm tổng số người trong danh bạ sinh vào tháng 12
+select COUNT(ma_nguoi) sinh_thang_12 from DanhBa where MONTH(ngay_sinh) = 12;
+--6c: Hiển thị thông tin về người của từng số điện thoại
+select * from DienThoai dt join DanhBa db on dt.ma_nguoi = db.ma_nguoi;
+--6d: Hiển thị toàn bộ thông tin về người có số điện thoại 123456789
+select * from DienThoai dt join DanhBa db on dt.ma_nguoi = db.ma_nguoi where so_dien_thoai = '123456789';
+--7a: Thay đổi trường ngày sinh là trước ngày hiện tại
+update DanhBa 
+set ngay_sinh = getdate() 
+where ngay_sinh > getdate();
+alter table DanhBa add constraint ngay_sinh_hien_tai check (ngay_sinh < getdate());
+--7c:Thêm trường ngày bắt đầu liên lạc
+alter table DienThoai add ngay_lien_lac date;
+--8a: Thực hiện các chỉ mục
+create index IX_HoTen on DanhBa(ho_va_ten);
+create index IX_SoDienThoai on DienThoai(so_dien_thoai);
+--8b: Viết các view
+--View_SoDienThoai
+create view View_SoDienThoai as select ho_va_ten, so_dien_thoai from DanhBa db join DienThoai dt on db.ma_nguoi = dt.ma_nguoi;
+--View_SinhNhat
+create view View_SinhNhat as
+select ho_va_ten,ngay_sinh, so_dien_thoai 
+from DanhBa db join DienThoai dt on db.ma_nguoi = dt.ma_nguoi 
+where MONTH(ngay_sinh) = MONTH(GETDATE());
+--8c: Viết các stored procedure
+--SP_Them_DanhBa
+create proc SP_Them_DanhBa @ma int, @ho_ten nvarchar(255), @dia_chi nvarchar(255), @ngay_sinh date
+as
+begin
+	insert into DanhBa (ma_nguoi, ho_va_ten, dia_chi, ngay_sinh) values (@ma, @ho_ten, @dia_chi, @ngay_sinh);
+end;
+--SP_Tim_DanhBa
+create proc SP_Tim_DanhBa @ho_ten nvarchar(255)
+as
+begin
+	select ho_va_ten, dia_chi, ngay_sinh, so_dien_thoai from DanhBa db join DienThoai dt on db.ma_nguoi = dt.ma_nguoi 
+	where ho_va_ten like @ho_ten;
+end;
---
+++ SQL/slot10_as6.sql
@@ -0,0 +1,121 @@
+﻿CREATE DATABASE BookStore
+ON PRIMARY
+    (NAME = bookstore,
+    FILENAME = 'bookstore_data.mdf',
+    SIZE = 4MB,
+    MAXSIZE = 4GB),
+FILEGROUP bookstore2
+    (NAME = bookstore2,
+    FILENAME = 'bookstore_data2.ndf',
+    SIZE = 2MB,
+    MAXSIZE = 2GB)
+LOG ON
+    (NAME = bookstore_log,
+    FILENAME = 'bookstore_log.ldf',
+    SIZE = 2MB,
+    MAXSIZE = 1GB);
+USE BookStore;
+CREATE TABLE Publishers (
+    MaNhaXuatBan INT PRIMARY KEY,
+    TenNhaXuatBan NVARCHAR(255),
+    DiaChiNhaXuatBan NVARCHAR(255)
+);
+
+CREATE TABLE BookTypes (
+    MaLoaiSach INT PRIMARY KEY,
+    TenLoaiSach NVARCHAR(255)
+);
+
+CREATE TABLE Books (
+    MaSach NVARCHAR(255) PRIMARY KEY,
+    TenSach NVARCHAR(255),
+    TacGia NVARCHAR(255),
+    NoiDungTomTat NVARCHAR(MAX),
+    NamXuatBan INT,
+    LanXuatBan INT,
+    GiaBan INT,
+    SoLuong INT,
+    MaNhaXuatBan INT FOREIGN KEY REFERENCES Publishers(MaNhaXuatBan),
+    MaLoaiSach INT FOREIGN KEY REFERENCES BookTypes(MaLoaiSach)
+);
+--2: Chèn dữ liệu
+INSERT INTO Publishers (MaNhaXuatBan, TenNhaXuatBan, DiaChiNhaXuatBan)
+VALUES (1, N'Tri Thức', N'53 Nguyễn Du, Hai Bà Trưng, Hà Nội'), (2, N'Kim Đồng', N'248 Trần Hưng Đạo, Hoàn Kiếm, Hà Nội'), (3, N'Giáo Dục', N'81 Trần Hưng Đạo, Hoàn Kiếm, Hà Nội');
+
+INSERT INTO BookTypes (MaLoaiSach, TenLoaiSach)
+VALUES (1, N'Khoa học xã hội'), (2, N'Toán học'), (3, N'Tin học');
+
+INSERT INTO Books (MaSach, TenSach, TacGia, NoiDungTomTat, NamXuatBan, LanXuatBan, GiaBan, SoLuong, MaNhaXuatBan, MaLoaiSach)
+VALUES (N'B001', N'Trí tuệ Do Thái', N'Eran Katz', N'Bạn có muốn biết: Người Do Thái sáng tạo ra cái gì và nguồn gốc trí tuệ của họ xuất phát từ đâu không? Cuốn sách này sẽ dần hé lộ những bí ẩn về sự thông thái của người Do Thái, của một dân tộc thông tuệ với những phương pháp và kỹ thuật phát triển tầng lớp trí thức đã được giữ kín hàng nghìn năm như một bí ẩn mật mang tính văn hóa.', 2010, 1, 79000, 100, 1, 1),
+	   (N'B002', N'Toán cao cấp', N'Nguyễn Văn A', N'Cuốn sách giới thiệu các khái niệm và phương pháp giải quyết bài toán trong toán cao cấp.', 2015, 1, 120000, 50, 2, 2),
+       (N'B003', N'Lập trình C++', N'Trần Văn B', N'Cuốn sách hướng dẫn lập trình C++ từ cơ bản đến nâng cao.', 2018, 1, 150000, 75, 3, 3);
+--3: Liệt kê cuốn sách có năm xuất bản từ 2008 đến nay
+SELECT TenSach, NamXuatBan FROM Books WHERE NamXuatBan >= 2008;
+--4: Liệt kê 10 cuốn sách có giá bán cao nhất
+SELECT TOP 10 * FROM Books ORDER BY GiaBan DESC;
+--5: Tìm những cuốn sách tiêu đề có từ tin học
+SELECT * FROM Books WHERE TenSach LIKE N'%tin học%';
+--6: Liệt kê các cuốn sách có tiêu đề bắt đầu bằng T theo thứ tự giá giảm dần
+SELECT * FROM Books WHERE TenSach LIKE N'T%' ORDER BY GiaBan DESC;
+--7: Liệt kê các cuốn sách của nhà xuất bản Tri thức
+SELECT * FROM Books B JOIN Publishers P ON B.MaNhaXuatBan = P.MaNhaXuatBan WHERE TenNhaXuatBan = N'Tri thức';
+--8: Lấy thông tin chi tiết về nhà xuất bản cuốn sách 'Trí tuệ do thái'
+SELECT TenNhaXuatBan, DiaChiNhaXuatBan FROM Publishers P JOIN Books B ON B.MaNhaXuatBan = P.MaNhaXuatBan WHERE TenSach = N'Trí tuệ Do Thái';
+--9: Hiển thị thông tin về cuốn sách
+SELECT MaSach, TenSach, NamXuatBan, TenNhaXuatBan, TenLoaiSach FROM Books B
+JOIN Publishers P ON B.MaNhaXuatBan = P.MaNhaXuatBan
+JOIN BookTypes T ON T.MaLoaiSach = B.MaLoaiSach;
+--10: Tìm cuốn sách có giá bán đắt nhất
+SELECT TOP 1 * FROM Books ORDER BY GiaBan DESC;
+--11: Tìm cuốn sách có số lượng lớn nhất trong kho
+SELECT TOP 1 * FROM Books ORDER BY SoLuong DESC;
+--12: Tìm các cuốn sách của tác giả 'Eran Katz'
+SELECT * FROM Books WHERE TacGia = N'Eran Katz';
+--13: Giảm giá bán 10% các cuốn sách xuất bản từ năm 2008 trở về trước
+UPDATE Books 
+SET GiaBan = GiaBan * 0.9
+WHERE NamXuatBan <= 2008;
+--14: Thống kê số đầu sách của mỗi nhà xuất bản
+SELECT TenNhaXuatBan, COUNT(MaSach) SoDauSach 
+FROM Books B JOIN Publishers P ON P.MaNhaXuatBan = B.MaNhaXuatBan 
+GROUP BY P.TenNhaXuatBan;
+--15: Thống kê số đầu sách của mỗi loại sách
+SELECT TenLoaiSach, COUNT(MaSach) SoLuong
+FROM Books B JOIN BookTypes T ON T.MaLoaiSach = B.MaLoaiSach
+GROUP BY TenLoaiSach;
+--16: Đặt chỉ mục cho trường tên sách
+CREATE INDEX ID_TenSach ON Books(TenSach);
+--17: Viết view lấy thông tin mã sách, tên sách, tác giả, nhà xuất bản và giá bán 
+CREATE VIEW ThongTin AS
+SELECT MaSach, TenSach, TacGia, TenNhaXuatBan, GiaBan
+FROM Books B JOIN Publishers P ON B.MaNhaXuatBan = P.MaNhaXuatBan;
+--18: Viết store procedure 
+--SP_Them_Sach: thêm mới một cuốn sách
+CREATE PROC SP_Them_Sach 
+@MaSach NVARCHAR(255), @TenSach NVARCHAR(255), 
+@TacGia NVARCHAR(255), @NoiDungTomTat NVARCHAR(MAX), 
+@NamXuatBan INT, @LanXuatBan INT, 
+@GiaBan INT, @SoLuong INT, 
+@MaNhaXuatBan INT, @MaLoaiSach INT
+AS
+BEGIN
+	INSERT INTO Books (MaSach, TenSach, TacGia, NoiDungTomTat, NamXuatBan, LanXuatBan, GiaBan, SoLuong, MaNhaXuatBan, MaLoaiSach)
+	VALUES (@MaSach, @TenSach, @TacGia, @NoiDungTomTat, @NamXuatBan, @LanXuatBan, @GiaBan, @SoLuong, @MaNhaXuatBan, @MaLoaiSach);
+END;
+EXEC SP_Them_Sach N'B004', N'Giải tích', N'Nguyễn Văn C', N'Cuốn sách giới thiệu các khái niệm và phương pháp giải quyết bài toán trong giải tích.', 2012, 1, 100000, 60, 2, 2;
+--SP_Tim_Sach: Tìm sách theo từ khóa
+CREATE PROC SP_Tim_Sach @TuKhoa NVARCHAR(255)
+AS
+BEGIN
+	SELECT * FROM Books WHERE TenSach LIKE N'%' + @TuKhoa + N'%';
+END;
+EXEC SP_Tim_Sach N'tích';
+--SP_Sach_ChuyenMuc: Liệt kê các cuốn sách theo mã chuyên mục
+CREATE PROC SP_Sach_ChuyenMuc @MaLoaiSach INT
+AS
+BEGIN
+    SELECT *
+    FROM Books
+    WHERE MaLoaiSach = @MaLoaiSach;
+END;
+EXEC SP_Sach_ChuyenMuc 2;
